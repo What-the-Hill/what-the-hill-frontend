520a47ed65511902be860f5236a7dfba3e37df21
Bloodyaugust <greysonrichey@gmail.com>
2025-02-25 09:07:37 -0700

    Add favicon and icons
    - Add `engines` entry for railway deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="WhatTheHill" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={`${roboto.variable} antialiased`}>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
